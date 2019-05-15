8e0b948d782c88a4e949baaba750e398c5bef6e8
raghav719 <sharm608@sheridancollege.ca>
2019-05-15 15:32:57 -0400

    Student-Demo

--- src/StudentDemo.java
+++ src/StudentDemo.java
@@ -2,11 +2,13 @@
 public class StudentDemo {
 
     private String name;
+    private int code;
 
     //constructor
-    public StudentDemo(String givenName)
+    public StudentDemo(String givenName, int givenCode)
     {
         name=givenName;
+        code=givenCode;
     }
     /**
      * @return the name
@@ -20,6 +22,20 @@ public class StudentDemo {
      */
     public void setName(String givenName) {
         name = givenName;
+    }
+
+    /**
+     * @return the code
+     */
+    public int getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(int code) {
+        this.code = code;
     }
     
     
